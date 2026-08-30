ebef98ef065d2e41ee84ac11c6a13cca576ffc49
Felipe Saint-Jean <fsaint@gmail.com>
2026-08-30 02:32:12 -0700

    test: fix Android boot recovery assertions

--- app/src/androidTest/kotlin/com/fsaint/androidagent/BootRecoveryTest.kt
+++ app/src/androidTest/kotlin/com/fsaint/androidagent/BootRecoveryTest.kt
@@ -7,8 +7,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 
 @RunWith(AndroidJUnit4::class)
 class BootRecoveryTest {
